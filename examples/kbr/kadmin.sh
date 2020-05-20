57e9e64c24fc6cabc050f04e33c364a434fe5a88
Akshat Gokhale <ajgokhale@gmail.com>
2020-05-19 21:05:53 -0400

    Working kbr auth service (large kadmin latency when starting service)

--- examples/kbr/kadmin.sh
+++ examples/kbr/kadmin.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sleep 10
-kadmin -p kadmin/service.example.com@EXAMPLE.COM -w yoda -q "ktadd -k /usr/local/var/service-keytab HTTP/kbr_service1_1.kbr_envoymesh"
+envoy -c /etc/service-envoy.yaml --service-cluster service${SERVICE_NAME} &
+while ! kadmin -p kadmin/service.example.com@EXAMPLE.COM -w yoda -q "ktadd -k /usr/local/var/service-keytab HTTP/kbr_service1_1.kbr_envoymesh"; do
+    echo "Trying to connect to admin server"
+done
+python3 /code/service.py
